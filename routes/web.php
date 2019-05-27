30be762fb84d3d3a82bb11450ff0b4993be689ce
jaimesandovalm <jaimesandovalm1990@gmail.com>
2019-05-27 15:56:36 -0400

    routes

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,27 @@
-<?php
+ <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/usuarios', function() {
+    return "usuarios";
+});
+
+Route::get('/usuarios/detalle',function(){
+    return 'Mostrando el usuario'.$_GET['id'];
+});
+
+/*se pasa por parametro dinamico*/
+Route::get('/usuarios2/{id}',function($id){
+    return 'Mostrando el id del usuario: '.$id;
+})->where('id','[0-9]+'); /*Aqui le especificamos que es solo numerico*/
+
+Route::get('usuarios/nuevo',function(){
+    return 'Crear nuevo usuario';
+});
+
+Route::get('/saludo/{name}/{nickname?}', function($name, $nickname = null){
+    if($nickname){
+        return "Bienvenido {$name}, tu apodo es {$nickname}";
+    }else{
+        return "Bienvenido {$name}, tu no tienes apodo";
+    }
 });
